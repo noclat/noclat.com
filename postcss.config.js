72015632d883ad08cadebafc71f8d1c2591ff704
noclat <nicolas.torres@algolia.com>
2019-05-17 02:02:20 +0200

    new website

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+const plugins = [
+  tailwindcss('./src/styles/tailwind.js')
+];
+
+if (process.env.NODE_ENV !== 'development') {
+  plugins.push(purgecss({
+    content: ['src/index.html'],
+    css: ['src/styles/main.css'],
+    extractors: [
+      {
+          extractor: TailwindExtractor,
+          extensions: ['html']
+      }
+    ]
+  }));
+}
+
+module.exports = {
+  plugins,
+};
